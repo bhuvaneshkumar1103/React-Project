9d10f864a840d41cfd616c997d195642c81aaa3b
BHUVANESH KUMAR.S <84184520+bhuvaneshkumar1103@users.noreply.github.com>
2022-03-23 21:25:56 +0530

    1st commit

---
+++ Use State-1/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NumberOps from './useState assignments/Use State-1/NumberOps';
+function App() {
+  return (
+    /* Calling X component with two values(a,b).
+       Calling Y component with Three values (a,b,c)
+       Calling Z component */
+    <>
+    <NumberOps/>
+    </>
+  );
+}
+
+export default App;
+
